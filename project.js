70f4e78c70473eca57c9a109b2c48b961415c015
Efeme-Royal <153027242+Efeme-Royal@users.noreply.github.com>
2023-12-05 22:34:46 +0100

    Create project.js

---
+++ project.js
@@ -0,0 +1,4 @@
+document.getElementById("button").onclick=function(){
+ let input=document.getElementById("text").value;
+document.getElementById("label").innerHTML="We wish you a Merry Christmas " + input;
+}
